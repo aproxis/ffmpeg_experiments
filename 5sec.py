e5ea472ea0f9604872142465599e15c642b3c42a
aproxis <aproxis@users.noreply.github.com>
2023-03-29 17:51:05 +0300

    Update 5sec.py
    This script will create a new folder for each input video in the output folder and save the 5-second segments in that folder with filenames like "input-1.mp4", "input-2.mp4", etc., where "input" is the original filename without the ".mp4" extension.

--- 5sec.py
+++ 5sec.py
@@ -3,7 +3,8 @@
 for file in /path/to/input/folder/*.mp4
 do
   base=$(basename "$file" .mp4)
-  mkdir "/path/to/output/folder/$base"
+  output_folder="/path/to/output/folder/$base"
+  mkdir -p "$output_folder"
 
   duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$file")
   segment_duration=5
@@ -12,6 +13,7 @@ do
   for i in $(seq 1 $segments)
   do
     start=$((i * segment_duration))
-    ffmpeg -i "$file" -ss 00:00:$start -t 00:00:$segment_duration -c copy "/path/to/output/folder/$base/$base-$i.mp4"
+    output_filename="$base-$i.mp4"
+    ffmpeg -i "$file" -ss 00:00:$start -t 00:00:$segment_duration -c copy "$output_folder/$output_filename"
   done
 done
